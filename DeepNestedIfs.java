38c49c37cca50fc0e4f17f0c7cab70dbbfa72aee
debu77er <72567041+debu77er@users.noreply.github.com>
2026-04-13 13:03:24 +0000

    Add files via upload

---
+++ DeepNestedIfs.java
@@ -0,0 +1,64 @@
+public class DeepNestedIfs {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int e = 50;
+        int f = 60;
+        int g = 70;
+        int h = 80;
+        int i = 90;
+        int j = 100;
+        int k = 110;
+        int l = 120;
+        int m = 130;
+        int n = 140;
+        int o = 150;
+        int p = 160;
+        int q = 170;
+        int r = 180;
+        int s = 190;
+        int t = 200;
+
+        if (a < b) {
+            if (b < c) {
+                if (c < d) {
+                    if (d < e) {
+                        if (e < f) {
+                            if (f < g) {
+                                if (g < h) {
+                                    if (h < i) {
+                                        if (i < j) {
+                                            if (j < k) {
+                                                if (k < l) {
+                                                    if (l < m) {
+                                                        if (m < n) {
+                                                            if (n < o) {
+                                                                if (o < p) {
+                                                                    if (p < q) {
+                                                                        if (q < r) {
+                                                                            if (r < s) {
+                                                                                if (s < t) {
+                                                                                    System.out.println("All conditions are true");
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
